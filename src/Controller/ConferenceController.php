094a293bcc0927e73e3e587357a63ab7c8c022d5
domas <domasw@gmail.com>
2022-06-15 17:39:23 +0300

    messenger async

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,45 +4,37 @@ namespace App\Controller;
 
 use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
+use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\CommentFormType;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use App\SpamChecker;
-
 
 class ConferenceController extends AbstractController
 {
-
     public function __construct(
         #[Autowire("%kernel.project_dir%/public/uploads/photos")]
         private string $photoDir,
-        private SpamChecker $spamChecker,
-        private EntityManagerInterface $entityManager
+        private MessageBusInterface $bus,
     ) {
     }
 
-
     #[Route('/', name: 'homepage')]
-    public function index(ConferenceRepository $conferenceRepository): Response
+    public function index(ConferenceRepository $conferences): Response
     {
-        return $this->render(
-            'conference/index.html.twig',
-            [
-                'conferences' => $conferenceRepository->findAll(),
-            ]
-        );
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $conferences->findAll(),
+        ]);
     }
 
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, SpamChecker $spamChecker): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
     {
-
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
 
@@ -57,33 +49,27 @@ class ConferenceController extends AbstractController
                 $comment->setPhotoFilename($filename);
             }
 
-            $this->entityManager->persist($comment);
+            $commentRepository->add($comment, true);
 
-            $context = [
+            $this->bus->dispatch(new CommentMessage($comment->getId(), [
                 'user_ip' => $request->getClientIp(),
                 'user_agent' => $request->headers->get('user-agent'),
                 'referrer' => $request->headers->get('referer'),
                 'permalink' => $request->getUri(),
-            ];
-            if (2 === $spamChecker->getSpamScore($comment, $context)) {
-                throw new \RuntimeException('Blatant spam, go away!');
-            }
-
-            $this->entityManager->flush();
+            ]));
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
         }
 
-        $offset = max(0, $request->query->getInt('offset'));
+        $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
         return $this->renderForm('conference/show.html.twig', [
-                'conference' => $conference,
-                'comments' => $paginator,
-                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
-                'comment_form' => $form,
-            ]
-        );
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form,
+        ]);
     }
 }
